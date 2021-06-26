30ce85a85f497d078b229b2082a69edca439bbb8
PinghuaZhuang <zphua2016@gmail.com>
2021-06-26 18:22:14 +0800

    feat: dingding.

--- packages/jsqpro/notify.js
+++ packages/jsqpro/notify.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const path = require('path')
 const Editor = require('utils/Editor')
 const marked = require('marked')
+const fetch = require('node-fetch')
 
 const args = {}
 process.argv.slice(2).forEach(k => {
@@ -21,6 +22,21 @@ const transporter = nodemailer.createTransport({
   },
 })
 
+async function sendDD() {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: raw,
+    redirect: 'follow'
+  }
+  await fetch('https://oapi.dingtalk.com/robot/send?access_token=de8411b704f9af18d09b2fda554a2ae370d5d816d596a9339dc925247bd69f48', requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(`>>> 发送钉钉机器人成功.`, result))
+    .catch(error => console.log('<<< 发送钉钉机器人失败.', error))
+}
+
 async function sendEmail() {
   const filePath = path.resolve(__dirname, '../../README.md')
   const options = {
@@ -30,8 +46,8 @@ async function sendEmail() {
   const { jsqpro } = Editor.structureObj(content)
 
   const mailOptions = {
-    from: "995382997@qq.com", // 发送邮箱必须与上面开通ssl的邮箱一致
-    to: "zphua2016@gmail.com", // 接收邮箱
+    from: '995382997@qq.com', // 发送邮箱必须与上面开通ssl的邮箱一致
+    to: 'zphua2016@gmail.com', // 接收邮箱
     subject: 'jsqpro 自动签到结果', // 标题
     //text: 'Hello world?', // text html二者选一
     html: marked(jsqpro.content),
@@ -41,9 +57,10 @@ async function sendEmail() {
     if (error) {
       console.log('<<< 发送失败！邮箱是否正确', error);
     } else {
-      console.log('>>> 发送成功! ', info);
+      console.log('>>> 发送邮件成功. ');
     }
   })
 }
 
+sendDD()
 sendEmail()
