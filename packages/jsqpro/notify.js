f8eead97e614ea8428da181404a8939c09200940
PinghuaZhuang <zphua2016@gmail.com>
2021-06-26 16:25:21 +0800

    feat: md => html.

--- packages/jsqpro/notify.js
+++ packages/jsqpro/notify.js
@@ -2,19 +2,20 @@ const nodemailer = require('nodemailer')
 const fs = require('fs')
 const path = require('path')
 const Editor = require('utils/Editor')
+const marked = require('marked')
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com', port: 465, secure: true, // use SSL
+  service: 'qq', // 使用内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
+  port: 465, // SMTP 端口
+  secureConnection: true, // 使用 SSL
   auth: {
-    name: 'Pinghua Zhuang',
-    user: 'zphua2016',
+    user: '995382997@qq.com',
     // 这里密码不是qq密码，是你设置的smtp授权码
     pass: process.env.JSQPRO_EMAIL_PASS,
   },
-  proxy: 'http://localhost:7890',
 })
 
-module.exports = async function () {
+async function sendEmail() {
   const filePath = path.resolve(__dirname, '../../README.md')
   const options = {
     encoding: 'utf8',
@@ -23,18 +24,20 @@ module.exports = async function () {
   const { jsqpro } = Editor.structureObj(content)
 
   const mailOptions = {
-    from: 'zphua2016@gmail.com', // 接收邮箱
-    to: '995382997@qq.com', // 发送邮箱必须与上面开通ssl的邮箱一致
+    from: "995382997@qq.com", // 发送邮箱必须与上面开通ssl的邮箱一致
+    to: "zphua2016@gmail.com", // 接收邮箱
     subject: 'jsqpro 自动签到结果', // 标题
     //text: 'Hello world?', // text html二者选一
-    html: jsqpro.content,
+    html: marked(jsqpro.content),
   }
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      console.log('发送失败！邮箱是否正确');
+      console.log('<<< 发送失败！邮箱是否正确');
     } else {
-      console.log(info);
+      console.log('>>> 发送成功! ', info);
     }
   })
 }
+
+sendEmail()
