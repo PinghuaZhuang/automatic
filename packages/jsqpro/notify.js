d26874e391cf49a34bb1518d651d540e86e06fba
PinghuaZhuang <zphua2016@gmail.com>
2021-06-26 16:15:00 +0800

    fix: action.

--- packages/jsqpro/notify.js
+++ packages/jsqpro/notify.js
@@ -15,14 +15,14 @@ const transporter = nodemailer.createTransport({
 })
 
 module.exports = async function () {
-  const filePath = path.resolve(__dirname, '../../../README.md')
+  const filePath = path.resolve(__dirname, '../../README.md')
   const options = {
     encoding: 'utf8',
   }
   let content = await fs.readFileSync(filePath, options)
   const { jsqpro } = Editor.structureObj(content)
 
-  const options = {
+  const mailOptions = {
     from: 'zphua2016@gmail.com', // 接收邮箱
     to: '995382997@qq.com', // 发送邮箱必须与上面开通ssl的邮箱一致
     subject: 'jsqpro 自动签到结果', // 标题
@@ -30,7 +30,7 @@ module.exports = async function () {
     html: jsqpro.content,
   }
 
-  transporter.sendMail(options, function (error, info) {
+  transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log('发送失败！邮箱是否正确');
     } else {
