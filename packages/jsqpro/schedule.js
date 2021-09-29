c2ee229d4356d30bd0228f75e3f75a0d5977c14e
zhuangpinghua <zphua2016@gmail.com>
2021-09-29 13:58:03 +0800

    feat: deloy action.

--- packages/jsqpro/schedule.js
+++ packages/jsqpro/schedule.js
@@ -6,8 +6,9 @@ const moment = require('moment')
 async function job() {
   const { signInTime } = await run(check)
   const delay = moment(signInTime).add(1, 'days') - moment()/*  + 1000 * 60 * 30 */
-  console.log(`>>> delay:`, delay, signInTime)
-  lt.setTimeout(job, delay > 0 ? delay : 1000 * 60 * 10)
+  const realDelay = delay > 0 ? delay : 1000 * 60 * 1
+  console.log(`>>> delay:`, realDelay, signInTime)
+  lt.setTimeout(job, realDelay)
 }
 
 job()
