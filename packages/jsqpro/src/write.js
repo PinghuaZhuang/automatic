c067fa845afeb989a798aa3c26fb6064554c9a2a
zhuangpinghua <zphua2016@gmail.com>
2021-09-29 18:24:12 +0800

    fix: moment.

--- packages/jsqpro/src/write.js
+++ packages/jsqpro/src/write.js
@@ -7,6 +7,10 @@ const DATE = 'YYYY-MM-DD HH:mm:ss'
 
 moment.locale('zh-cn')
 
+// const timesReg = /\<\!-- checked:(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2};)* --\>/
+const timesReg = /\<\!-- checked:(?<times>.*) --\>/
+
+
 module.exports = async function job(signInTime = '2021-01-01', inviteAddress = 'https://registered.jsqpro.store/auth/register') {
   console.log(`>>> 更新 README.md.`, signInTime)
   const filePath = path.resolve(__dirname, '../../../README.md')
@@ -18,7 +22,7 @@ module.exports = async function job(signInTime = '2021-01-01', inviteAddress = '
   const today = moment().format(DATE)
 
   if (jsqpro) {
-    let times = /\<\!-- checked:(?<times>[^\s]*) --\>/.exec(content)
+    let times = timesReg.exec(content)
     jsqpro.content = Editor.replaceWeek(Editor.signInTxt())
     if (times) {
       // 每月1日重置
@@ -26,7 +30,7 @@ module.exports = async function job(signInTime = '2021-01-01', inviteAddress = '
         await fs.writeFileSync(path.resolve(__dirname, `../log/${moment().month()}.txt`), times.join(';\n'), options)
         times = []
       } else {
-        times = times.groups.times.replace(/;$/, '').split(';')
+        times = times.groups && times.groups.times.replace(/;$/, '').split(';') || []
       }
       if (signInTime !== '-1') {
         times.push(moment(signInTime).format(DATE))
@@ -41,7 +45,7 @@ module.exports = async function job(signInTime = '2021-01-01', inviteAddress = '
         console.log(`>>> check table:`, jsqpro.content)
         return
       }
-      jsqpro.content = jsqpro.content.replace(/\<\!-- checked:([^\s]*) --\>/, `<!-- checked:${times.join(';')} -->`)
+      jsqpro.content = jsqpro.content.replace(timesReg, `<!-- checked:${times.join(';')} -->`)
       times.forEach(time => {
         jsqpro.content = Editor.replaceCheck(jsqpro.content, time)
       })
